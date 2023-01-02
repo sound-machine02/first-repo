e779719c955d9446b7bcc87cea08b06a08601356
A D <clansletsclash840@outlook.com>
2023-01-02 22:33:07 +0530

    modified

--- Second.java
+++ Second.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.util.*;
 public class Second {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two values: ");
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int sum = x+y;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
         System.out.println("Sum = "+sum);
     }
 }
